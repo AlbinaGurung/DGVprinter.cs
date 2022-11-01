f9ca9917a1155c534f14bab3f2deff2c7fa8004b
Albina Gurung <albinakushu9545@gmail.com>
2022-11-01 10:46:24 -0700

    second commit

--- Printer1/Form1.Designer.cs
+++ Printer1/Form1.Designer.cs
@@ -28,13 +28,168 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.savebtn = new System.Windows.Forms.Button();
+            this.printbtn = new System.Windows.Forms.Button();
+            this.ProductNametbx = new System.Windows.Forms.TextBox();
+            this.TotalPricetbx = new System.Windows.Forms.TextBox();
+            this.Quantitytbx = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.dgv1 = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // savebtn
+            // 
+            this.savebtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.savebtn.Location = new System.Drawing.Point(242, 160);
+            this.savebtn.Name = "savebtn";
+            this.savebtn.Size = new System.Drawing.Size(115, 51);
+            this.savebtn.TabIndex = 0;
+            this.savebtn.Text = "SAVE";
+            this.savebtn.UseVisualStyleBackColor = true;
+            this.savebtn.Click += new System.EventHandler(this.savebtn_Click);
+            // 
+            // printbtn
+            // 
+            this.printbtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.printbtn.Location = new System.Drawing.Point(367, 160);
+            this.printbtn.Name = "printbtn";
+            this.printbtn.Size = new System.Drawing.Size(115, 51);
+            this.printbtn.TabIndex = 1;
+            this.printbtn.Text = "PRINT";
+            this.printbtn.UseVisualStyleBackColor = true;
+            this.printbtn.Click += new System.EventHandler(this.printbtn_Click);
+            // 
+            // ProductNametbx
+            // 
+            this.ProductNametbx.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ProductNametbx.Location = new System.Drawing.Point(267, 29);
+            this.ProductNametbx.Name = "ProductNametbx";
+            this.ProductNametbx.Size = new System.Drawing.Size(255, 26);
+            this.ProductNametbx.TabIndex = 2;
+            // 
+            // TotalPricetbx
+            // 
+            this.TotalPricetbx.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TotalPricetbx.Location = new System.Drawing.Point(267, 91);
+            this.TotalPricetbx.Name = "TotalPricetbx";
+            this.TotalPricetbx.Size = new System.Drawing.Size(255, 26);
+            this.TotalPricetbx.TabIndex = 3;
+            // 
+            // Quantitytbx
+            // 
+            this.Quantitytbx.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Quantitytbx.Location = new System.Drawing.Point(267, 59);
+            this.Quantitytbx.Name = "Quantitytbx";
+            this.Quantitytbx.Size = new System.Drawing.Size(255, 26);
+            this.Quantitytbx.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(151, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(110, 20);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Product Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(151, 62);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(68, 20);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Quantity";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(151, 94);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(83, 20);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Total Price";
+            // 
+            // dgv1
+            // 
+            this.dgv1.AllowUserToAddRows = false;
+            this.dgv1.AllowUserToDeleteRows = false;
+            this.dgv1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgv1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgv1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3});
+            this.dgv1.Location = new System.Drawing.Point(72, 243);
+            this.dgv1.Name = "dgv1";
+            this.dgv1.ReadOnly = true;
+            this.dgv1.Size = new System.Drawing.Size(672, 195);
+            this.dgv1.TabIndex = 8;
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "ProductName";
+            this.Column1.Name = "Column1";
+            this.Column1.ReadOnly = true;
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Quantity";
+            this.Column2.Name = "Column2";
+            this.Column2.ReadOnly = true;
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "TotalPrice";
+            this.Column3.Name = "Column3";
+            this.Column3.ReadOnly = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dgv1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Quantitytbx);
+            this.Controls.Add(this.TotalPricetbx);
+            this.Controls.Add(this.ProductNametbx);
+            this.Controls.Add(this.printbtn);
+            this.Controls.Add(this.savebtn);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgv1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button savebtn;
+        private System.Windows.Forms.Button printbtn;
+        private System.Windows.Forms.TextBox ProductNametbx;
+        private System.Windows.Forms.TextBox TotalPricetbx;
+        private System.Windows.Forms.TextBox Quantitytbx;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.DataGridView dgv1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
     }
 }
 
--- Printer1/Form1.cs
+++ Printer1/Form1.cs
@@ -8,6 +8,9 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+using DGVPrinterHelper;
+
+
 namespace Printer1
 {
     public partial class Form1 : Form
@@ -16,5 +19,30 @@ namespace Printer1
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void printbtn_Click(object sender, EventArgs e)
+        {
+       DGVPrinter printer = new DGVPrinter();
+            printer.Title="DataGridView Report";
+            printer.SubTitle=String.Format("Products", printer.SubTitleColor=Color.Black,printer);
+            printer.SubTitleFormatFlags=StringFormatFlags.LineLimit|StringFormatFlags.NoClip;
+            printer.PageNumberInHeader=false;
+            printer.PorportionalColumns=true;
+            printer.Footer="List of products";
+            printer.FooterSpacing=1;
+            printer.PrintPreviewDataGridView(dgv1);
+            
+        }
+
+        private void savebtn_Click(object sender, EventArgs e)
+        {
+            dgv1.Rows.Add(ProductNametbx.Text,Quantitytbx.Text,TotalPricetbx.Text);
+        }
     }
+
 }
